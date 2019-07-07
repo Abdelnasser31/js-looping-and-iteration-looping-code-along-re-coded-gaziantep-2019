8e038d4d89655548e0e19bf4ac7c71e05c58e2d4
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-07 19:32:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+
+function writeCards (names, event){
+  const messages = [];
+  for (let i = 0 ; i < names.length ; i++){
+    messages[i] = `Thanks ${names[i]} for your swift gift on this ${event} `;
+  }
+  return messages;
+}
